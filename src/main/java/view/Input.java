2cf11560ea18cf743403d675f0facb4542a8987f
siru02 <khm32323@gmail.com>
2024-03-31 22:30:40 +0900

    playTimeInput

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -1,5 +1,6 @@
 package view;
 
+import controller.IntegerParser;
 import controller.util;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,7 +8,7 @@ import java.util.Scanner;
 
 public class Input {
     private static final String CAR_INPUT =  "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String CNT = "시도할 회수는 몇회인가요?";
+    private static final String TRY_CNT = "시도할 회수는 몇회인가요?";
     private static Scanner scanner = new Scanner(System.in);
     String  inputString;
     String  inputStringSplit[];
@@ -16,12 +17,14 @@ public class Input {
         inputString = scanner.nextLine();
         return (util.splitByDelemeter(inputString, ","));
     }
-    void playTimeInput() {
-        int cnt;
-        while (true){
-            System.out.println(CNT);
-            cnt = scanner.nextInt();
-
+    int playTimeInput() {
+        try {
+            System.out.println(TRY_CNT);
+            String input = scanner.nextLine();
+            return IntegerParser.parse(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return playTimeInput(); //while문 없이 재귀형식으로 반복입력도 가능하다
         }
     }
 }
